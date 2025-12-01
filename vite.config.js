943a53bb2650220f3558546587d95f000945b2b7
Bilal-S <bilal.soylu[at]gmail.com>
2025-12-01 16:37:47 -0500

    icon fixes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['german-coach-icon.svg', 'icons/ios/180.png'],
       manifest: {
         name: 'A1 German Coach - Vocabulary Training',
         short_name: 'German Coach',
